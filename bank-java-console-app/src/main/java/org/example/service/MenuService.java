16ee1df1b6b0adb043b99f7e540f26b3fe8c21d8
JUKKarol <karolprzybylak2@gmail.com>
2025-02-17 22:28:53 +0100

    Add print menu

---
+++ bank-java-console-app/src/main/java/org/example/service/MenuService.java
@@ -0,0 +1,42 @@
+package org.example.service;
+
+import java.util.Scanner;
+
+public class MenuService {
+    public static void printMenu() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the banking system!");
+        System.out.println("1. Check account balance");
+        System.out.println("2. Make a transfer");
+        System.out.println("3. Deposit funds");
+        System.out.println("4. Withdraw funds");
+        System.out.println("5. Exit");
+        System.out.print("Choose an option: ");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Checking account balance...");
+                break;
+            case 2:
+                System.out.println("Making a transfer...");
+                break;
+            case 3:
+                System.out.println("Depositing funds...");
+                break;
+            case 4:
+                System.out.println("Withdrawing funds...");
+                break;
+            case 5:
+                System.out.println("Thank you for using our system!");
+                break;
+            default:
+                System.out.println("Invalid choice, please try again.");
+                printMenu();
+        }
+
+        scanner.close();
+    }
+}
