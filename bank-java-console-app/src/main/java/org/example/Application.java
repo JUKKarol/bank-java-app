2ed389d0f0c91a0c626ebbc6367b7c1e8fede487
JUKKarol <karolprzybylak2@gmail.com>
2025-02-26 22:07:13 +0100

    Add Spring Boot

--- bank-java-console-app/src/main/java/org/example/Application.java
+++ bank-java-console-app/src/main/java/org/example/Application.java
@@ -7,19 +7,29 @@ import java.util.ArrayList;
 import static org.example.service.MenuService.invokeMenu;
 import static org.example.service.MenuService.login;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class Application implements CommandLineRunner {
     public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) {
         ArrayList<User> users = new ArrayList<>();
         users.add(new User("user1", "1", 1000, "1"));
         users.add(new User("user2", "2", 1000, "2"));
 
-        while (true)
-        {
+        while (true) {
             String loggedUserAccountNumber = login(users);
             System.out.println(loggedUserAccountNumber);
 
-            if (loggedUserAccountNumber != null)
-            {
+            if (loggedUserAccountNumber != null) {
                 boolean exit = invokeMenu(users, loggedUserAccountNumber);
                 if (exit) {
                     break;
