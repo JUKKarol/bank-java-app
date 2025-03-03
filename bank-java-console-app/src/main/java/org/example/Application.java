0d2e3101a63176eef06e4a69213cd5c90d3ee81a
JUKKarol <karolprzybylak2@gmail.com>
2025-03-03 21:29:18 +0100

    Add more users

--- bank-java-console-app/src/main/java/org/example/Application.java
+++ bank-java-console-app/src/main/java/org/example/Application.java
@@ -24,6 +24,8 @@ public class Application implements CommandLineRunner {
         ArrayList<User> users = new ArrayList<>();
         users.add(new User("user1", "1", 1000, "1"));
         users.add(new User("user2", "2", 1000, "2"));
+        users.add(new User("user3", "3", 1000, "3"));
+        users.add(new User("user4", "4", 1000, "4"));
 
         while (true) {
             String loggedUserAccountNumber = login(users);
