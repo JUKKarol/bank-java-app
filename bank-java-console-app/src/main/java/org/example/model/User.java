ed547b51f6f6cca6bbe8ff86c119e298c62e431f
JUKKarol <karolprzybylak2@gmail.com>
2025-03-19 15:19:27 +0100

    Add packages for security and jwt

--- bank-java-console-app/src/main/java/org/example/model/User.java
+++ bank-java-console-app/src/main/java/org/example/model/User.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 @Getter
 @Setter
@@ -16,9 +20,29 @@ import lombok.Setter;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(unique = true, length = 100, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private Integer  balance;
+
+    @Column(unique = true, nullable = false)
     private String accountNumber;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
 }
