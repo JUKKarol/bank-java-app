b10379f8f6a326d2dc2ddc620a765f1ac4decdfc
JUKKarol <karolprzybylak2@gmail.com>
2025-03-28 15:48:47 +0100

    Fix path in secCfg

--- bank-java-console-app/src/main/java/org/example/configs/SecurityConfiguration.java
+++ bank-java-console-app/src/main/java/org/example/configs/SecurityConfiguration.java
@@ -33,7 +33,7 @@ public class SecurityConfiguration {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
