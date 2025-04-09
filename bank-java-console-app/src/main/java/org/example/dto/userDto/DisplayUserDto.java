ee11ad9160a821c82011a3a7a0e56ea8de8f502f
JUKKarol <karolprzybylak2@gmail.com>
2025-04-09 06:46:27 +0200

    Add DisplayUserDto

---
+++ bank-java-console-app/src/main/java/org/example/dto/userDto/DisplayUserDto.java
@@ -0,0 +1,28 @@
+package org.example.dto.userDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DisplayUserDto {
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Integer  balance;
+
+    private String accountNumber;
+
+    private Date createdAt;
+
+    private Date updatedAt;
+}
