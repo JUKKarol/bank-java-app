12bcf1e392b4bac5380f6e93c13c6182c1b95871
JUKKarol <karolprzybylak2@gmail.com>
2025-05-28 07:01:20 +0200

    Fix mapper

--- auth-service/src/main/java/com/github/jukkarol/mapper/UserMapper.java
+++ auth-service/src/main/java/com/github/jukkarol/mapper/UserMapper.java
@@ -3,14 +3,11 @@ package com.github.jukkarol.mapper;
 import com.github.jukkarol.dto.userDto.DisplayUserDto;
 import com.github.jukkarol.model.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
     DisplayUserDto userToDisplayUserDto(User user);
 
     List<DisplayUserDto> usersToDisplayUserDtos(List<User> users);
