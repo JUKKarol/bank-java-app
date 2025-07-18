7f34b3b5f7378891bc0ce4fec3bb2e768bfd9b7d
JUKKarol <karolprzybylak2@gmail.com>
2025-07-19 00:57:11 +0200

    Split cash controller

--- atm-service/src/main/java/com/github/jukkarol/controller/DepositController.java
+++ atm-service/src/main/java/com/github/jukkarol/controller/DepositController.java
@@ -2,8 +2,6 @@ package com.github.jukkarol.controller;
 
 import com.github.jukkarol.dto.depositDto.request.MakeDepositRequest;
 import com.github.jukkarol.dto.depositDto.response.MakeDepositResponse;
-import com.github.jukkarol.dto.withdrawalDto.request.MakeWithdrawalRequest;
-import com.github.jukkarol.dto.withdrawalDto.response.MakeWithdrawalResponse;
 import com.github.jukkarol.service.DepositService;
 import com.github.jukkarol.service.WithdrawalService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,7 +11,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,13 +20,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("cash")
+@RequestMapping("deposit")
 @PreAuthorize("hasRole('ATM')")
-public class CashController {
+public class DepositController {
     private final DepositService depositService;
     private final WithdrawalService withdrawalService;
 
-    @PostMapping("deposit")
+    @PostMapping
     @Operation(
             summary = "Make deposit",
             description = "Make deposit for a specified user"
@@ -53,34 +50,6 @@ public class CashController {
     })
     public ResponseEntity<MakeDepositResponse> makeDeposit(@RequestBody @Valid MakeDepositRequest request) {
         MakeDepositResponse response = depositService.makeDeposit(request);
-
-        return ResponseEntity.ok(response);
-    }
-
-    @PostMapping("withdrawal")
-    @Operation(
-            summary = "Make withdrawal",
-            description = "Make withdrawal for a specified user"
-    )
-    @ApiResponses(value = {
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Withdrawal finished successfully",
-                    content = @Content(
-                            mediaType = "application/json",
-                            schema = @Schema(implementation = MakeWithdrawalResponse.class)
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Validation error",
-                    content = @Content(
-                            mediaType = "application/json"
-                    )
-            ),
-    })
-    public ResponseEntity<MakeWithdrawalResponse> makeWithdrawal(@RequestBody @Valid MakeWithdrawalRequest request) {
-        MakeWithdrawalResponse response = withdrawalService.makeWithdrawal(request);
 
         return ResponseEntity.ok(response);
     }
