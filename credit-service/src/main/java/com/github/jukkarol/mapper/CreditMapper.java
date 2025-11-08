a60eefb9db53b56db343192dcad6320b3b8f06e2
JUKKarol <karolprzybylak2@gmail.com>
2025-11-08 21:57:24 +0100

    Add mapper for credit

---
+++ credit-service/src/main/java/com/github/jukkarol/mapper/CreditMapper.java
@@ -0,0 +1,10 @@
+package com.github.jukkarol.mapper;
+
+import com.github.jukkarol.dto.creditDto.request.CreateCreditRequest;
+import com.github.jukkarol.model.Credit;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CreditMapper {
+    Credit createCreditRequestToCredit(CreateCreditRequest request);
+}
