6df15eb48809e7841b04e0625e3a37b25382dbab
JUKKarol <karolprzybylak2@gmail.com>
2025-11-19 17:59:17 +0100

    Update docker to java 25

--- init-db.sql
+++ init-db.sql
@@ -1,7 +1,9 @@
 CREATE DATABASE "auth-service";
 CREATE DATABASE "transaction-service";
 CREATE DATABASE "atm-service";
+CREATE DATABASE "credit-service";
 
 GRANT ALL PRIVILEGES ON DATABASE "auth-service" TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE "transaction-service" TO postgres;
 GRANT ALL PRIVILEGES ON DATABASE "atm-service" TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE "credit-service" TO postgres;
