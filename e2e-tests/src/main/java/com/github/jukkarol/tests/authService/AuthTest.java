269fa77f48a2b7841c1c672d3c421aa13cf62937
JUKKarol <karolprzybylak2@gmail.com>
2026-04-03 07:30:25 +0200

    Add login with bad credentials test

--- e2e-tests/src/main/java/com/github/jukkarol/tests/authService/AuthTest.java
+++ e2e-tests/src/main/java/com/github/jukkarol/tests/authService/AuthTest.java
@@ -1,4 +1,4 @@
-package com.github.jukkarol.tests;
+package com.github.jukkarol.tests.authService;
 
 import com.github.jukkarol.clients.AuthApiClient;
 import io.restassured.response.Response;
@@ -14,14 +14,28 @@ class AuthTest {
     @Autowired
     private AuthApiClient authApiClient;
 
+    String email = "usertest123@gmail.com";
+    String name = "user";
+    String password = "password!123";
+
     @Test
     void shouldReturnTokenOnValidCredentials() {
-        Response responseSignup = authApiClient.signup("user@gmail.com", "user", "password");
+        Response responseSignup = authApiClient.signup(email, name, password);
         assertThat(responseSignup.statusCode()).isIn(200, 409);
 
-        Response responseLogin = authApiClient.login("user@gmail.com", "password");
+        Response responseLogin = authApiClient.login(email, password);
 
         assertThat(responseLogin.statusCode()).isEqualTo(200);
         assertThat(responseLogin.jsonPath().getString("token")).isNotBlank();
+    }
+
+    @Test
+    void shouldCanNotLoginWithInvalidCredentials() {
+        Response responseSignup = authApiClient.signup(email, name, password);
+        assertThat(responseSignup.statusCode()).isIn(200, 409);
+
+        Response responseLogin = authApiClient.login(email, password+"1");
+
+        assertThat(responseLogin.statusCode()).isEqualTo(401);
     }
 }
